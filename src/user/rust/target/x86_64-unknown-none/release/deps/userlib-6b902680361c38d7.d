86f8e7d567d5dee159ad01b78fa09b62f21460e9
Anfer <3392594480@qq.com>
2026-05-18 22:46:15 +0800

    chore: .gitignore 添加 src/user/rust/target/, 移除误提交的构建产物和测试报告

--- src/user/rust/target/x86_64-unknown-none/release/deps/userlib-6b902680361c38d7.d
+++
@@ -1,9 +0,0 @@
-/home/anfer/Code/AntX/src/user/rust/target/x86_64-unknown-none/release/deps/userlib-6b902680361c38d7.d: src/lib.rs src/syscall.rs src/install_wizard.rs
-
-/home/anfer/Code/AntX/src/user/rust/target/x86_64-unknown-none/release/deps/libuserlib-6b902680361c38d7.rlib: src/lib.rs src/syscall.rs src/install_wizard.rs
-
-/home/anfer/Code/AntX/src/user/rust/target/x86_64-unknown-none/release/deps/libuserlib-6b902680361c38d7.rmeta: src/lib.rs src/syscall.rs src/install_wizard.rs
-
-src/lib.rs:
-src/syscall.rs:
-src/install_wizard.rs:
